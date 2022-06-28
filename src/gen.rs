c18430ec2baf126a71f79551407860f175da7c64
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-06-28 16:25:57 -0400

    Create `random_num()` and `random_char()` functions

--- src/gen.rs
+++ src/gen.rs
@@ -1,4 +1,18 @@
 #![allow(unused, dead_code)]
-use std::sync::Mutex;
+use std::{sync::Mutex, ops::Index};
+use rand::prelude::*;
 
+const CHARS: &str = "abcdefghijklmnopqrstuvwxyz1234567890";
 static USED: Mutex<Vec<String>> = Mutex::new(vec![]);
+
+
+fn random_num(rng: &ThreadRng, min: i32, mut max: i32) -> i32 {
+	max += 1;
+	let alpha: f32 = rand::random::<f32>();
+	min + ((max - min) as f32 * alpha) as i32
+}
+
+fn random_char(rng: &ThreadRng) -> char {
+	let idx: usize = random_num(rng, 0, CHARS.len() as i32) as usize;
+	CHARS.bytes().collect::<Vec<u8>>()[idx] as char
+}
