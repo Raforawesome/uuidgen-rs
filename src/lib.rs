cd393de835c5acce85c66048708ba45d6a56e3ae
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-06-28 16:46:09 -0400

    Write id and uuid tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
 mod gen;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub use gen::{ gen_uuid, gen_id };
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	fn test_id() {
+		let id: String = gen_id(false);
+		let id2: String = gen_id(true);
+		println!("Non-dashed: {id}\nDashed: {id2}")
+	}
+
+	#[test]
+	fn test_uuid() {
+		let id: String = gen_uuid(false);
+		let id2: String = gen_uuid(true);
+		println!("Non-dashed: {id}\nDashed: {id2}")
+	}
 }
